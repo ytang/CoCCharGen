1348f78be7afdcdeedf45d93daddc686d7575a3e
Yang Tang <tangyang.cn@gmail.com>
2012-12-30 15:21:28 -0500

    Handle screen rotation.

--- src/com/example/cocchargen/MainActivity.java
+++ src/com/example/cocchargen/MainActivity.java
@@ -12,6 +12,16 @@ import java.util.Random;
 
 public class MainActivity extends Activity {
     private final Random rand = new Random();
+    private int strength;
+    private int constitution;
+    private int power;
+    private int dexterity;
+    private int appearance;
+    private int size;
+    private int intelligence;
+    private int education;
+    private int damageBonus;
+    private int income;
 
     /**
      * Called when the activity is first created.
@@ -60,23 +70,26 @@ public class MainActivity extends Activity {
         }
     }
 
-    void generateCharacter() {
-        int strength = roll(3, 6, 0);
-        int constitution = roll(3, 6, 0);
-        int power = roll(3, 6, 0);
-        int dexterity = roll(3, 6, 0);
-        int appearance = roll(3, 6, 0);
-        int size = roll(2, 6, 6);
-        int intelligence = roll(2, 6, 6);
-        int education = roll(3, 6, 3);
+    void rollDices() {
+        strength = roll(3, 6, 0);
+        constitution = roll(3, 6, 0);
+        power = roll(3, 6, 0);
+        dexterity = roll(3, 6, 0);
+        appearance = roll(3, 6, 0);
+        size = roll(2, 6, 6);
+        intelligence = roll(2, 6, 6);
+        education = roll(3, 6, 3);
+        damageBonus = lookupDamageBonus(strength + size);
+        income = roll(1, 10, 0) - 1;
+    }
+
+    void populateList() {
         int sanity = power * 5;
         int idea = intelligence * 5;
         int luck = power * 5;
         int know = education * 5;
-        int damageBonus = lookupDamageBonus(strength + size);
         int hp = (constitution + size + 1) / 2;
         int mp = power;
-        int income = roll(1, 10, 0) - 1;
         String[] income1890s = {"500", "1000", "1500", "2000", "2500", "3000", "4000", "5000", "5000", "10,000"};
         String[] income1920s = {"1500", "2500", "3500", "3500", "4500", "5500", "6500", "7500", "10,000", "20,000"};
         String[] incomePresent = {"15,000", "25,000", "35,000", "45,000", "55,000", "75,000", "100,000", "200,000", "300,000", "500,000"};
@@ -107,5 +120,43 @@ public class MainActivity extends Activity {
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.characteristic, characteristics);
         ((ListView) findViewById(R.id.listView)).setAdapter(adapter);
+    }
+
+    void generateCharacter() {
+        rollDices();
+        populateList();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("strength", strength);
+        outState.putInt("constitution", constitution);
+        outState.putInt("power", power);
+        outState.putInt("dexterity", dexterity);
+        outState.putInt("appearance", appearance);
+        outState.putInt("size", size);
+        outState.putInt("intelligence", intelligence);
+        outState.putInt("education", education);
+        outState.putInt("damageBonus", damageBonus);
+        outState.putInt("income", income);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null) {
+            strength = savedInstanceState.getInt("strength");
+            constitution = savedInstanceState.getInt("constitution");
+            power = savedInstanceState.getInt("power");
+            dexterity = savedInstanceState.getInt("dexterity");
+            appearance = savedInstanceState.getInt("appearance");
+            size = savedInstanceState.getInt("size");
+            intelligence = savedInstanceState.getInt("intelligence");
+            education = savedInstanceState.getInt("education");
+            damageBonus = savedInstanceState.getInt("damageBonus");
+            income = savedInstanceState.getInt("income");
+            populateList();
+        }
     }
 }
